f8e04277ef5e253087345333c72a90534d22e273
Maxwell <mxt.developer@gmail.com>
2019-07-08 16:27:49 +0200

    Remove Deprecated References

--- support-auth-facebook/src/main/java/co/anitrend/support/auth/facebook/FacebookInitProvider.kt
+++ support-auth-facebook/src/main/java/co/anitrend/support/auth/facebook/FacebookInitProvider.kt
@@ -9,38 +9,36 @@ import co.anitrend.support.auth.core.extension.getMetaValue
 
 class FacebookInitProvider : ContentProvider() {
 
-  private fun initFacebook() {
-    val facebookAppId = context?.getMetaValue(R.string.co_anitrend_support_auth_facebookId)
-    if (facebookAppId != null && facebookAppId.isNotEmpty()) {
-      FacebookSdk.setApplicationId(facebookAppId)
-      FacebookSdk.sdkInitialize(context)
-      FacebookSdk.setWebDialogTheme(android.R.style.Theme_Holo_Light_NoActionBar)
+    private fun initFacebook() {
+        val facebookAppId = context?.getMetaValue(R.string.co_anitrend_support_auth_facebookId)
+        if (facebookAppId != null && facebookAppId.isNotEmpty()) {
+            FacebookSdk.setApplicationId(facebookAppId)
+        }
     }
-  }
 
-  override fun onCreate(): Boolean {
-    initFacebook()
-    return false
-  }
+    override fun onCreate(): Boolean {
+        initFacebook()
+        return false
+    }
 
-  override fun query(uri: Uri, projection: Array<String>?, selection: String?, selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
-    return null
-  }
+    override fun query(uri: Uri, projection: Array<String>?, selection: String?, selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
+        return null
+    }
 
-  override fun getType(uri: Uri): String? {
-    return null
-  }
+    override fun getType(uri: Uri): String? {
+        return null
+    }
 
-  override fun insert(uri: Uri, values: ContentValues?): Uri? {
-    return null
-  }
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
 
-  override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-    return 0
-  }
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        return 0
+    }
 
-  override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
-    return 0
-  }
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
+        return 0
+    }
 
 }
