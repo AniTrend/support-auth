f99da40cd3ddc7f70252e92d403c3f2194bb2966
Maxwell <mxt.developer@gmail.com>
2019-08-02 21:43:56 +0200

    Refactor Facebook Module

--- support-auth-facebook/src/main/java/co/anitrend/support/auth/facebook/FacebookAuthActivity.kt
+++ support-auth-facebook/src/main/java/co/anitrend/support/auth/facebook/FacebookAuthActivity.kt
@@ -23,33 +23,32 @@ import co.anitrend.support.auth.core.view.DialogFactory
 import co.anitrend.support.auth.core.CoreAuthActivity
 import co.anitrend.support.auth.core.model.SocialUser
 import co.anitrend.support.auth.core.extension.isFacebookInstalled
+import co.anitrend.support.auth.facebook.presenter.FacebookPresenter
 
-class FacebookAuthActivity : CoreAuthActivity(), FacebookCallback<LoginResult>, GraphRequest.GraphJSONObjectCallback {
+internal class FacebookAuthActivity : CoreAuthActivity() {
+
+    override val authenticationMeta by lazy {
+        AuthCache.instance.facebookAuthenticationMeta
+    }
 
     private var callbackManager: CallbackManager? = null
     private var loadingDialog: ProgressDialog? = null
 
-    private val scopes: List<String> by lazy {
-        Arrays.asList("email", "public_profile")
-    }
+    private val scopes = listOf("email", "public_profile")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        loadingDialog = DialogFactory.createLoadingDialog(this)
-
         callbackManager = CallbackManager.Factory.create()
+        loadingDialog = DialogFactory.createLoadingDialog(this)
+        val facebookPresenter = FacebookPresenter(this, loadingDialog)
 
         if (isFacebookInstalled())
             LoginManager.getInstance().logOut()
 
-        LoginManager.getInstance().registerCallback(callbackManager, this)
+        LoginManager.getInstance().registerCallback(callbackManager, facebookPresenter)
 
         LoginManager.getInstance().logInWithReadPermissions(this, scopes)
-    }
-
-    override val authenticationMeta by lazy {
-        AuthCache.instance.facebookAuthenticationMeta
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -57,42 +56,7 @@ class FacebookAuthActivity : CoreAuthActivity(), FacebookCallback<LoginResult>, 
         callbackManager?.onActivityResult(requestCode, resultCode, data)
     }
 
-    override fun onSuccess(loginResult: LoginResult) {
-        loadingDialog?.show()
-        val request = GraphRequest.newMeRequest(loginResult.accessToken, this)
-        val parameters = Bundle()
-        parameters.putString("fields", "id,name,email,link")
-        request.parameters = parameters
-        request.executeAsync()
-    }
-
-    override fun onCancel() {
-        onCancellation()
-    }
-
-    override fun onError(error: FacebookException) {
-        onExceptionThrown(error)
-        if (error is FacebookAuthorizationException) {
-            LoginManager.getInstance().logOut()
-        }
-    }
-
-    override fun onCompleted(`object`: JSONObject, response: GraphResponse) {
-        val user = SocialUser().apply {
-            userId = `object`.optString("id", "")
-            accessToken = AccessToken.getCurrentAccessToken().token
-            profilePictureUrl = String.format(PROFILE_PIC_URL, userId)
-            email = `object`.optString("email", "")
-            fullName = `object`.optString("name", "")
-            pageLink = `object`.optString("link", "")
-        }
-        loadingDialog?.dismiss()
-        onSocialSuccess(user)
-    }
-
     companion object {
-
-        private const val PROFILE_PIC_URL = "https://graph.facebook.com/%1\$s/picture?type=large"
 
         fun start(context: Context?) {
             val intent = Intent(context, FacebookAuthActivity::class.java)
